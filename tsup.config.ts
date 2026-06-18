3f5ee170bb0f26574b7ca3a4c0beb65ee293e245
AveryOn <lnkonion2@gmail.com>
2026-06-18 21:48:16 +0400

    init git hooks by husky && init ts && esm && typecheck

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/main.ts"],
+  format: ["esm"],
+  target: "node24",
+  platform: "node",
+  outDir: "dist",
+  clean: true,
+  sourcemap: true,
+  splitting: false,
+  bundle: true,
+  minify: false,
+  dts: false,
+  treeshake: true
+});
