76980ac59208395f787d054483cc95d7665b26c0
AveryOn <lnkonion2@gmail.com>
2026-06-19 10:27:42 +0400

    [channels] add repo port

---
+++ src/modules/v1/channels/ports/channels.repo.port.ts
@@ -0,0 +1,16 @@
+import type {
+  Channel,
+  CreateChannelInput,
+  UpdateChannelInput
+} from '~/modules/v1/channels/domain/channels.types'
+
+export abstract class ChannelsRepositoryPort {
+  abstract findAll(): Promise<Channel[]>
+  abstract findById(id: string): Promise<Channel | null>
+  abstract findByCode(code: string): Promise<Channel | null>
+  abstract create(input: CreateChannelInput): Promise<Channel>
+  abstract update(
+    id: string,
+    input: UpdateChannelInput
+  ): Promise<Channel | null>
+}
