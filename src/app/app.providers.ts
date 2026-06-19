2661cdc4e0e40a78afa3a093575fa895b76bef84
AveryOn <lnkonion2@gmail.com>
2026-06-19 09:36:10 +0400

    connect idempotency module to app providers

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,16 +1,18 @@
+import type { DiProvider } from '~/core/di/types'
+
 import {
   ENV_TOKEN,
   HTTP_SERVER_TOKEN,
   LOGGER_TOKEN
 } from '~/core/app/app.tokens'
 import { env } from '~/env'
-import type { DiProvider } from '~/core/di/types'
 import { databaseProviders } from '~/infra/database/database.providers'
 import { logger } from '~/infra/logger/logger.factory'
 import { HttpServer } from '~/infra/transport/http/http.server'
 import { preferencesProviders } from '~/modules/v1/preferences/preferences.module'
 import { quietHoursProviders } from '~/modules/v1/quiet-hours/quiet-hours.module'
 import { globalPoliciesProviders } from '~/modules/v1/global-policies/global-policies.module'
+import { idempotencyProviders } from '~/modules/v1/idempotency/idempotency.module'
 
 export const appProviders: DiProvider[] = [
   { token: ENV_TOKEN, useValue: env },
@@ -19,5 +21,6 @@ export const appProviders: DiProvider[] = [
   ...quietHoursProviders,
   ...preferencesProviders,
   ...globalPoliciesProviders,
+  ...idempotencyProviders,
   { token: HTTP_SERVER_TOKEN, useClass: HttpServer }
 ]
